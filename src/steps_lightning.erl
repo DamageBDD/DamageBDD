31095a0a4adeef08adf3d166c4a2aa42d4d9a5ed
Steven Joseph <steven@damagebdd.com>
2024-09-14 10:37:59 +1000

    Added step to pay lightning request

---
+++ src/steps_lightning.erl
@@ -0,0 +1,23 @@
+-module(steps_lightning).
+
+-author("Steven Joseph <steven@stevenjoseph.in>").
+
+-copyright("Steven Joseph <steven@stevenjoseph.in>").
+
+-license("Apache-2.0").
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("kernel/include/logger.hrl").
+
+-export([step/6]).
+
+step(
+  _Config,
+  Context,
+  <<"Then">>,
+  _N,
+  ["I pay the invoice with payment request", PaymentRequest],
+  _
+) ->
+  Result = lnd:pay_invoice(PaymentRequest),
+  maps:put(lightning_payment_status, Result, Context).
