cc499858e26b4831c78fc74ffd7d75861146baf7
Steven Joseph <steven@damagebdd.com>
2024-09-14 10:39:07 +1000

    Added step skeleton for ai behavior testing
    - to do implement

---
+++ src/steps_ai.erl
@@ -0,0 +1,27 @@
+-module(steps_ai).
+
+-author("Steven Joseph <steven@stevenjoseph.in>").
+
+-copyright("Steven Joseph <steven@stevenjoseph.in>").
+
+-license("Apache-2.0").
+
+-include_lib("kernel/include/logger.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+-export([step/6]).
+
+step(Config, Context, <<"And">>, _N, ["I am using ai ", AiProvider], Args) ->
+  ?LOG_DEBUG("and config: ~p context: ~p  args: ~p", [Config, AiProvider, Args]),
+  Context;
+
+step(
+  Config,
+  Context,
+  <<"And">>,
+  _N,
+  ["I set the messages context", GptContext],
+  Args
+) ->
+  ?LOG_DEBUG("and config: ~p context: ~p  args: ~p", [Config, GptContext, Args]),
+  Context.
