63c7b8f41e24642cb3f7dd3329a7efdfde621a86
Steven Joseph (asyncmind) <steven@asyncmind.xyz>
2025-04-10 09:01:01 +1000

    fix: update arch packages install core-lightinig
    - start bech32

--- bin/arch_packages.sh
+++ bin/arch_packages.sh
@@ -5,4 +5,19 @@ sudo pacman -S --needed \
      kubo \
      bitcoin-daemon \
      libyaml
+
+if ! command -v yay &> /dev/null; then
+  echo "yay not found. Installing yay..."
+  sudo pacman -S --needed git base-devel
+  git clone https://aur.archlinux.org/yay-bin.git
+  cd yay-bin
+  makepkg -si
+  cd ..
+  rm -rf yay-bin
+else
+  echo "yay is already installed."
+fi
+
+yay -S \
+    core-lightning
      
