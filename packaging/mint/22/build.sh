71429ca96e11d56c0202c14ced574feddc7b9cd4
Steven Joseph <steven@damagebdd.com>
2025-10-29 14:41:30 +1100

    fix: updated debian package builds

--- packaging/mint/22/build.sh
+++ packaging/mint/22/build.sh
@@ -2,20 +2,27 @@
 #  --build-arg APT_BUSTER=$(date +%s) \
 #  --build-arg REBAR3_URL=https://s3.amazonaws.com/rebar3/rebar3 \
 #  -t damagebdd/rebar3-mint:latest .
-DOCKER_BUILDKIT=1 docker build -t damagebdd/mint22-builder:latest .
+set -e
 
+DOCKER_BUILDKIT=1 docker build \
+  --build-arg CACHEBUST=$(date +%s) \
+  -t damagebdd/mint22-builder:latest .
+#rm -f deb/*.deb
 docker run --rm -it \
   -v "$(pwd)/deb:/out" \
   -w /opt/workspace \
   damagebdd/mint22-builder:latest \
   bash -lc '
     set -e
+    git reset --hard
     # optional, only if this is actually a git clone:
     if [ -d .git ]; then git pull --ff-only || true; fi
 
     rm -f rebar.lock
     rm -rf _build
 
+    #bash
+    DEBUG=1
     rebar3 as prod release
 
     # package with your plugin
@@ -24,4 +31,5 @@ docker run --rm -it \
     # copy debs to host
     cp -a _build/pkg/deb/*.deb /out/
     rm -f rebar.lock
+
   '
--- packaging/mint/22/run.sh
+++ packaging/mint/22/run.sh
@@ -1,11 +1,20 @@
-docker run --rm -it \
+  #-v "$(pwd)/../../../:/opt/workspace" \
+docker run  -it \
   -v "$(pwd)/deb:/deb" \
   -w /opt/workspace \
-  damagebdd/mint22-builder:latest \
+    -p 8888:8080 \
+  linuxmintd/mint22-amd64 \
   bash -xlc '
  set -e
     #apt-get update -y
-    apt-get install -y /deb/damage_*.deb     # install your deb package
-    dpkg -L damage
+
+
+    PKG_DEBUG=1 dpkg -i -D3 /deb/damage_*.deb
+
+    #apt-get install -y /deb/damage_*.deb
+    #dpkg -L damage
+    export SHELL=sh
+    bash
     /opt/damage/bin/damage foreground
+
 '
