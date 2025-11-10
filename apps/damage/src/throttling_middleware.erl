dd86d9084e24bf3b86cb9e2c0e2b0e53efdbd982
Steven Joseph <steven@damagebdd.com>
2025-11-10 23:28:19 +1100

    fix: add whitelisting for throttling

--- apps/damage/src/throttling_middleware.erl
+++ apps/damage/src/throttling_middleware.erl
@@ -8,11 +8,24 @@
 execute(Req, Env) ->
     IP = damage_utils:get_ip(Req),
 
-    case throttle:check(public_read, IP) of
-        {limit_exceeded, _, _} ->
-            ?LOG_WARNING("IP ~p exceeded api limit", [IP]),
-            Req3 = cowboy_req:reply(429, Req),
-            {stop, Req3};
-        _ ->
-            {ok, Req, Env}
+    %% Load whitelist from sys.config (e.g. {ip_whitelist, ["127.0.0.1"]})
+    Whitelist =
+        case application:get_env(throttle, ip_whitelist) of
+            {ok, List} when is_list(List) -> List;
+            _ -> []
+        end,
+
+    %% Skip throttle if IP is whitelisted
+    case lists:member(IP, Whitelist) of
+        true ->
+            {ok, Req, Env};
+        false ->
+            case throttle:check(public_read, IP) of
+                {limit_exceeded, _, _} ->
+                    ?LOG_WARNING("IP ~p exceeded api limit", [IP]),
+                    Req3 = cowboy_req:reply(429, Req),
+                    {stop, Req3};
+                _ ->
+                    {ok, Req, Env}
+            end
     end.
