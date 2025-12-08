d0b7652bdb650aa304f3f38caa3adef7f0faeabe
Steven Joseph <steven@damagebdd.com>
2025-12-09 09:50:29 +1100

    fix(ecai): use point to construct filename

--- apps/ecai/src/ecai.erl
+++ apps/ecai/src/ecai.erl
@@ -57,6 +57,12 @@ init() ->
 hash_to_curve(_Arg) -> erlang:nif_error(nif_library_not_loaded).
 
 curve_add(_X1, _Y1, _X2, _Y2) -> erlang:nif_error(nif_library_not_loaded).
+-spec point_to_filename_hash({integer(), integer()}) -> binary().
+point_to_filename_hash({X, Y}) ->
+    Bin = term_to_binary({X, Y}),
+    Hash = crypto:hash(sha256, Bin),
+    Slug = base32:encode(binary:part(Hash, 0, 20)),
+    <<"ecai_", Slug/binary, ".wav">>.
 
 mint_knowledge(
     #{public_key := AeAccount, private_key := _PrivateKey} = KeyPair,
@@ -68,15 +74,16 @@ mint_knowledge(
     } = Knowledge
 ) ->
     EncodedKnowledge = encode(Knowledge),
-    Point = {_X, _Y} = ecai:hash_to_curve(EncodedKnowledge),
-    MetaData = #{Point => ecai:hash_to_curve(EncodedKnowledge)},
+    Point = ecai:hash_to_curve(EncodedKnowledge),
+    {ok, Hash} = ipfs:add({data, EncodedKnowledge, point_to_filename_hash(Point)}),
+    MetaData = #{point => Point, ipfs_hash => Hash},
 
     damage_ae:contract_call(
         KeyPair,
         ct_id(#{}),
         "contracts/knowledge_nft.aes",
         "mint",
-        [AeAccount, MetaData, Knowledge]
+        [AeAccount, MetaData, Hash]
     ).
 
 encode(#{subject := Subject, predicate := Predicate, object := Object, context := Context}) ->
